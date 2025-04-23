01f51305fdc3362b3841b04fbf97ed75c05b52e7
pankaj-deepnapsoftech <itsybizz@gmail.com>
2025-04-23 12:22:33 +0530

    some chnages

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -9,35 +9,54 @@ export const CreateUser = AsyncHandler(async (req, res) => {
     const find = await UserModle.find({});
     if (find.length !== 0) {
         return res.status(400).json({
-            message: "admin already login"
+            message: "admin already register"
         });
     };
     const newUser = await UserModle.create(data);
     return res.status(201).json({
-        message:"user created successful",
-        data:newUser
+        message: "user created successful",
+        data: newUser
     })
 });
 
-export const login  = AsyncHandler(async (req,res) => {
-    const {email,password} = req.body;
-    const find = await UserModle.findOne({email});
-    if(!find){
+export const login = AsyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+    const find = await UserModle.findOne({ email });
+    if (!find) {
         return res.status(400).json({
-            message:"bad credentials"
+            message: "bad credentials"
         });
     };
 
-    
-    if(password != find.password){
+
+    if (password != find.password) {
         return res.status(400).json({
-            message:"bad credentials 1"
+            message: "bad credentials 1"
         });
     };
 
-    const token = GenerateToken({email:find.email},'1day')
-    res.cookie("tk",token,{httpOnly:true,secure:config.NODE_ENV !== "development",sameSite:"none",maxAge:1000*60*60*24})
+    const token = GenerateToken({ email: find.email }, '1day')
+    res.cookie("tk", token, { httpOnly: true, secure: config.NODE_ENV !== "development", sameSite: "none", maxAge: 1000 * 60 * 60 * 24 })
     return res.status(200).json({
-        message:"login successful"
+        message: "login successful"
     })
-})
+});
+
+export const logout = AsyncHandler(async (_req, res) => {
+    return res.clearCookie('tk').status(200).json({
+        message: 'Logout Successful',
+    });
+});
+
+export const logedInUser = AsyncHandler(async (req,res) => {
+    const user = await UserModle.findOne({})
+    return res.status(200).json({
+        data:user
+    })
+});
+
+
+
+
+
+
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,5 @@
 import {Router} from "express";
-import { CreateUser, login } from "../controllers/user.controller.js";
+import { CreateUser, logedInUser, login, logout } from "../controllers/user.controller.js";
 import { Validator } from "../helper/validator.js";
 import { AuthRegisterValidation } from "../validation/auth.validation.js";
 
@@ -8,6 +8,8 @@ const router  = Router();
 
 router.route("/register").post(Validator(AuthRegisterValidation),CreateUser);
 router.route("/login").post(Validator(AuthRegisterValidation),login);
+router.route("/logout").get(logout);
+router.route("/logedin-user").get(logedInUser);
 
 
 export default router;
