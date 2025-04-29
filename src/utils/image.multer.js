a5ae84d6679397b191c0bf565910f46b6cd10752
pankaj-deepnapsoftech <itsybizz@gmail.com>
2025-04-29 15:28:52 +0530

    some changes

--- src/utils/image.multer.js
+++ src/utils/image.multer.js
@@ -10,7 +10,7 @@ const __dirname = dirname(__filename);
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, path.join(__dirname, './tmp/uploads'));
+        cb(null, path.join(__dirname, '../public/temp'));
     },
     filename: function (req, file, cb) {
         cb(null, `${file.fieldname}-${uuidv4()}${path.extname(file.originalname)}`);
